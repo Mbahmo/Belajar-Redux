6b85f5b5875b6099e60b4634f0534088637e92a0
Mbahmo <anthonyleembahmo@gmail.com>
2020-01-04 20:12:37 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="Logo"/>
+          <h2>Redux</h2>
+        </div>
+        <div className="Todo-App">
+          <form>
+            <input type="text"/>
+          </form>
+          <div className="Todo-List">
+            <ul>
+              {this.props.todos.map(todo => (
+                <li key={todo.id}>
+                  <input type="checkbox" defaultChecked={todo.isComplete}/>{todo.name}
+                </li>
+              ))}
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
